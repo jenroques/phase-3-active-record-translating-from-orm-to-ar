c16c8ed7f0f7cac4eaaa7ca9273b1c07818838c2
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2022-10-17 19:27:51 -0500

    commit tree f9b9941f16b1c82b9dbdd12bdc4b3f3e7c92d5e3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,9 @@
-class Dog
+require "pry"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/test.sqlite"
+)
+class Dog < ActiveRecord::Base
 end
